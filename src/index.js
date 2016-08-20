1022c5e58f432ea1be8ea9c846f56e867d90bedc
Anders Olavessen <Anders Olavessen>
2016-08-19 19:06:19 -0500

    publish

--- src/index.js
+++ src/index.js
@@ -5,21 +5,21 @@ import CharacterListView from 'character-list-view';
 const id = 563;
 
 export default function () {
-  fetch(`http://gateway.marvel.com:80/v1/public/series/${id}?apikey=e5758327dc82abb92b83ef12f459ff4e`)
+  fetch(`http://gateway.marvel.com:80/v1/public/series/${id}?apikey=68706a532b6461683940ee2900eeccb5`)
     .then((response) => response.json())
     .then((info) => {
       const element = document.querySelector(`.series-details-sidebar`);
       const profile = new SeriesInfoView(element, info.data.results[0]);
     });
 
-  fetch(`http://gateway.marvel.com:80/v1/public/series/${id}/characters?apikey=e5758327dc82abb92b83ef12f459ff4e`)
+  fetch(`http://gateway.marvel.com:80/v1/public/series/${id}/characters?apikey=68706a532b6461683940ee2900eeccb5`)
     .then((response) => response.json())
     .then((info) => {
       const element = document.querySelector(`.characters`);
       const character = new CharacterListView(element, info.data.results);
     });
 
-  fetch(`http://gateway.marvel.com:80/v1/public/series/${id}/comics?apikey=e5758327dc82abb92b83ef12f459ff4e`)
+  fetch(`http://gateway.marvel.com:80/v1/public/series/${id}/comics?apikey=68706a532b6461683940ee2900eeccb5`)
       .then((response) => response.json())
       .then((info) => {
         const element = document.querySelector(`.comics`);
